b4ee88fbcd5d15c4c56b15673ff790e4e32cab38
Mohd Adib <relesafe@gmail.com>
2018-01-23 22:07:33 +0800

    update stats counts

---
+++ app/assets/javascripts/statcounter.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+var sc_project=11608651; 
+var sc_invisible=1; 
+var sc_security="de8648a7"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
